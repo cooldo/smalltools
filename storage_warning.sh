c2bf88cfc9ba99351713930cdd8d9f14c716c5cf
cooldo <qianggao11@163.com>
2019-11-09 11:52:26 +0800

    add awk to make script more readable

--- storage_warning.sh
+++ storage_warning.sh
@@ -7,10 +7,12 @@ show_usage() {
     exit 1
 }
 
-STORAGE="sda9"
+STORAGE="sda10"
 
 notify_user() {
-    TTY=$(who | tr -s ' '  | cut -d' ' -f 2)
+    #TTY=$(who | tr -s ' '  | cut -d' ' -f 2)
+    # better way
+    TTY=$(who | awk '{ print $2 }')
     for tty in $TTY
     do
         echo -e "\033[31m "$1"\033[0m" > "/dev/$tty"
@@ -21,8 +23,19 @@ if [[ $# -ne 1 ]];then
     show_usage
 fi
 
-PERCENTAGE=$(df | sed -n "/"$STORAGE"/p" | tr -s ' ' | cut -d' ' -f 5)
+#PERCENTAGE=$(df | sed -n "/"$STORAGE"/p" | tr -s ' ' | cut -d' ' -f 5)
+# best way
+#PERCENTAGE=$(df | awk '{ if($1 ~ /sda10/)  print $5 }')
+# better way
+PERCENTAGE=$(df | awk '/sda10/ { print $5 }')
+# awk usage
+# awk '/[0-9]/ { print }
+# awk '/[0-9]$/ { print }
+# awk '/^[0-9]/ { print }
+# awk -F: '/^[0-9]/ { print }
+
 NUMBER=${PERCENTAGE%\%*}
+echo $NUMBER
 
 if [[ "$NUMBER" -gt $1 ]];then
     notify_user "STORAGE FULL $NUMBER%"
