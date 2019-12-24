7dabffda52c29313491aaf117722fb489dfb4cd0
cooldo <qianggao11@163.com>
2019-12-25 00:31:48 +0800

    compress bash_history

---
+++ history_compress.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os
+import shutil
+history = os.path.expandvars('$HOME') + '/.bash_history'
+history_compressed = os.path.expandvars('$HOME') + '/.bash_history_compressed'
+# store ~/.bash_history into a list
+history_list = []
+# store compressed history into a list
+history_compressed_list = []
+with open(history, 'r') as f:
+	for line in f:
+		history_list.append(line)
+	history_list.reverse()
+
+	for item in history_list:
+		if item not in history_compressed_list:
+			history_compressed_list.append(item)
+	history_compressed_list.reverse()
+
+with open(history_compressed, 'w') as f:
+	for item in history_compressed_list:
+		f.write(item)
+
+shutil.move(history_compressed, history)
